8d327e740ed9ef89527762923597d07a65b950d1
bonillab <nestorbonillabrenes@gmail.com>
2017-07-05 18:26:21 -0600

    Pantalla principal

--- js/activity.js
+++ js/activity.js
@@ -1,10 +1,27 @@
-define(["sugar-web/activity/activity"], function (activity) {
+
+define(function (require) {
+        var activity = require("sugar-web/activity/activity");
+        var datastore = require("sugar-web/datastore");
+        var jquery = require("jquery");
+        var Mustache = require("mustache");
+        var tpl = require("../js/tpl.js");
+        
 
 	// Manipulate the DOM only when it is ready.
 	require(['domReady!'], function (doc) {
+        var out = "";
 
 		// Initialize the activity.
 		activity.setup();
+        
+        $(document).ready(function(){
+	    out = Mustache.render(tpl[0].tpl);
+	    $('#canvas').html(out);
+	    });
+        
+        $('#canvas').on('click','#btn_inicio',function(){
+           console.log("key press"); 
+        });
 
 	});
 
---
+++ js/tpl.js
@@ -0,0 +1,5 @@
+define(function (require) {
+    return [
+        {'tpl' : '<div id="itf_1"><button id="btn_inicio" type="button">Click</button></div>'},
+    ];
+});
