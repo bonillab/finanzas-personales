522959634fd20670656aa24b38f0fee9c506c1bb
bonillab <nestorbonillabrenes@gmail.com>
2017-07-09 23:28:27 -0600

    agregando icono

--- js/activity.js
+++ js/activity.js
@@ -3,8 +3,25 @@ define(function (require) {
         var activity = require("sugar-web/activity/activity");
         var datastore = require("sugar-web/datastore");
         var jquery = require("jquery");
+        var sweetalert = require("sweetalert");
         var Mustache = require("mustache");
         var tpl = require("../js/tpl.js");
+    
+    function nulo(num){
+        if(isNaN(num)) return 0;
+        else return num;
+    }
+    
+    function suma(valor){
+        var list = new Array();
+        var suma = 0;
+        for(var i=0;i<2;i++){
+            var val = nulo(parseFloat($('#'+valor+(i+1)).val()));
+            list.push(val);
+            suma += list[i];
+        }
+        return suma;
+    }
     
 // Manipulate the DOM only when it is ready.
 	require(['domReady!'], function (doc) {
@@ -31,6 +48,19 @@ define(function (require) {
         
         $('#canvas').on('click','#btn_jugar',function(){
            mustache(2); 
+        });
+        
+        /*Pintar en juego 1*/
+        $('#canvas').on('click','#btn_acep',function(){
+            var total_nec = suma('nec');
+            var total_des = suma('des');
+            if(total_des > total_nec){
+                swal({  title : "¿Realmente tus deseos son más importantes que tus necesidades?",
+                        type: "warning"            
+                    });
+               }
+            $('#tot_nec').html('<h1>'+total_nec+'</h1>');
+            $('#tot_des').html('<h1>'+total_des+'</h1>');
         });
 
 	});
--- js/tpl.js
+++ js/tpl.js
@@ -1,7 +1,6 @@
 define(function (require) {
     return [
         {'tpl' : '<div class="inter_size" id="itf_1"><button class="button" id="btn_inicio" type="button"></button></div>'},
-        {'tpl' : '<div class="inter_size" id="int_2"><button class="button" id="btn_historia"></button><button class="button" id="btn_jugar"></button></div>'},
-        {'tpl' : '<div class="inter_size" id="int_3"></div>'}
+        {'tpl' : '<div class="inter_size" id="int_2"><button class="button" id="btn_historia"></button><button class="button" id="btn_jugar"></button></div>'}
     ];
 });
